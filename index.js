a35f465a75aa8db3f641fd4454ae9ea491df5feb
DonaldCao <dongge.cao@siemens-healthineers.com>
2021-01-28 17:26:42 +0800

    add express

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const http = require('http');
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    var sn = req.getQueryStringValue("sn");
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/query', function(req, res) {
+    const sn = req.query["sn"];
     var obj = {name:"CT02", sn : sn, description : "this is a test CT", };
-    res.end(obj);
-}
+    res.send(obj);
+});
 
-const server = http.createServer(requestListener);
-server.listen(443);
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
